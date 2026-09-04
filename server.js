ccdc7f51bed401e816d77594861d806998a00d52
kailash <kailash@gmail.com>
2026-09-05 02:02:13 +0530

    Fix CORS origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require('./config/db');
 const app = express();
 
 app.use(cors({
-  origin: "https://news-app-sigma-blond.vercel.app/",
+  origin: "https://news-app-sigma-blond.vercel.app",
   credentials: true
 }));
 app.use(express.json());
