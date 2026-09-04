946fb325faf8b59675a675f82fb88cf3044fcbe7
kailash <kailash@gmail.com>
2026-09-05 01:54:22 +0530

    Fix CORS for deployed frontend

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ const articleActionRoutes = require("./routes/articleActionRoutes");
 const connectDB = require('./config/db');
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://news-app-kailash18.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
